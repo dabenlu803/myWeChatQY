7c6bd88efb2dd840757b3fc52941dcadb0c56699
li.l <li.l@ireadygo.com>
2016-03-28 18:02:45 +0800

    add wechat verify
    Change-Id: Ia3c404647664f15423419f529a90ad4c461ae288

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from wechat_sdk import WechatConf
+from wechat_sdk import WechatBasic
 
 # Create your views here.
+def requestHandler(request):
+    if (request.method == 'POST'):
+        response = HttpResponse(check_valid(request), content_type="text/plain")
+    return response
+
+
+def check_valid(request):
+    global TOKEN
+    signature = request.GET.get("signature", None)
+    timestamp = request.GET.get("timestamp", None)
+    nonce = request.GET.get("nonce", None)
+    echostr = request.GET.get("echostr", None)
+    wechat = init_wechat_sdk()
+    if wechat.check_signature(signature, timestamp, nonce):
+        return echostr
+    else:
+        return None
+
+
+def init_wechat_sdk():
+    conf = WechatConf(
+        token='pHmNrfXUFhKlpScFA1vYjTIZr',
+        appid='wx9f17765d31c60590',
+        appsecret='-akqs0eCRlzg8ZLxexjV0Z9z8eCIuwU-azj--LVyDJ6BID_-XYQQ_g9rXJhvovCD',
+        encrypt_mode='safe',
+        encoding_aes_key='WlT2qlLR6VBTDzPuTpLzPeL4aGd6OyrtaBKLhzQ8Dwj'
+    )
+    return WechatBasic(conf)
