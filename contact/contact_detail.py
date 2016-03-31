9f07dbe5bb2f18738268fbcb907f641fa2ddb5d4
li.l <li.l@ireadygo.com>
2016-03-31 18:15:52 +0800

    fix bug of display user detail without attrs
    Change-Id: Ib6dfc74b17035b692e1f3a002e1dfb63626c00ff

--- contact/contact_detail.py
+++ contact/contact_detail.py
@@ -12,5 +12,7 @@ def show_contact_detail(request, param):
     # client = WeChatClient(wechat_config.CORP_ID, wechat_config.SECRET)
     # stuff = client.user.get(param)
     stuff = UserInfo.objects.get(user_id=param)
-    attrs = eval(stuff.extr_attr).get('attrs')
+    attrs = []
+    if stuff.extr_attr != '':
+        attrs = eval(stuff.extr_attr).get('attrs', None)
     return render_to_response('contact_detail.html', {'item': stuff, 'attrs': attrs})
